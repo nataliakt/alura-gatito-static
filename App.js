66f9480cceb6717f4bebe4f5de403051fbdb7f2a
Natalia Kelim Thiel <natalia.kthiel@gmail.com>
2021-01-10 15:39:06 -0300

    Aula 2.1

--- App.js
+++ App.js
@@ -1,21 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import Servicos from './src/telas/Servicos';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Alura!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Servicos />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
