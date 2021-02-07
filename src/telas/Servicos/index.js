bf88580ea48fbf201dc12745b4f95fb2631066f7
Natalia Kelim Thiel <natalia.kthiel@gmail.com>
2021-02-07 16:21:23 -0300

    Aula 4.2

--- src/telas/Servicos/index.js
+++ src/telas/Servicos/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 import { FlatList } from 'react-native';
 
-import TelaPadrao from '../../componentes/TelaPadrao';
 import Item from './Item';
 
 const servicos = [
@@ -27,11 +26,11 @@ const servicos = [
 ];
 
 export default function Servicos() {
-  return <TelaPadrao>
+  return <>
     <FlatList
       data={servicos}
       renderItem={({item}) => <Item {...item}  />}
       keyExtractor={({id}) => String(id)}
     />
-  </TelaPadrao>
+  </>
 }
