8108817fe7a3bc7b96f7ceb23e9c94882b89a2d7
Natalia Kelim Thiel <natalia.kthiel@gmail.com>
2021-03-07 21:17:55 -0300

    Aula 5.1

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import 'intl';
 import 'intl/locale-data/jsonp/pt-BR';
+import Reactotron from 'reactotron-react-native';
 
 import TelaPadrao from './src/componentes/TelaPadrao';
 import Rotas from './src/Rotas';
 
+Reactotron.configure().useReactNative().connect();
+console.tron = Reactotron;
+
 export default function App() {
+  console.tron.log({
+    curso: "alura"
+  });
   return <TelaPadrao><Rotas /></TelaPadrao>;
 }
